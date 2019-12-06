a103abfe50c84cbd38d67012f5926db8ccffd3df
bboystatix <statix231@hotmail.com>
2019-12-06 22:07:57 +0800

    Set up basic click broadcast hook to be triggered on button click

--- src/Containers/Client/index.tsx
+++ src/Containers/Client/index.tsx
@@ -1,9 +1,32 @@
 import React, {FC} from "react";
+import {gql} from "apollo-boost"
+import {useLazyQuery} from '@apollo/react-hooks';
+
+const BROADCAST_CLICK = gql`
+    query broadcastClick($timestamp: String!, $type: Colour!) {
+        broadcastClick(timestamp: $timestamp, type: $type) {
+            success
+            message       
+        }
+    }
+`
+
 
 const Client: FC = () => {
+    const [broadcastClick, {loading, error, data}] = useLazyQuery(BROADCAST_CLICK);
+    if (loading) return (<div>Loading...</div>);
+    if (error) return <div>{`Error! ${error.message}`}</div>;
+
     return (
         <div className='client'>
-            Client
+            <button
+                onClick={() => broadcastClick(
+                    {variables: {timestamp: `${Date.now()}`, type: 'blue'}}
+                    )
+                }
+            >
+                Client
+            </button>
         </div>
     )
 }
