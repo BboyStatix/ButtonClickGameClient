5378b57548ab052979eaf21df029ce2d2173608a
bboystatix <statix231@hotmail.com>
2019-12-06 22:51:04 +0800

    Set up styles for the buttons

---
+++ src/Containers/Client/Styles.ts
@@ -0,0 +1,18 @@
+export const styles = {
+    button: {
+        borderRadius: "50%",
+        borderWidth: 0,
+        color: "#fff",
+        fontSize: "50px",
+        height: "250px",
+        width: "250px",
+        margin: "10px",
+        cursor: "pointer"
+    },
+    orangeButton: {
+        backgroundColor: '#ff9559',
+    },
+    blueButton: {
+        backgroundColor: '#007aff',
+    }
+}
