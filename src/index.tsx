6b642a557d27415032bcf3f9db826bf09d8c8bf5
bboystatix <statix231@hotmail.com>
2019-12-06 20:26:52 +0800

    Hook up ApolloProvider and pass in client

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './Containers/App';
 import {BrowserRouter as Router} from "react-router-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks"
+const client = new ApolloClient({
+    uri: "http://localhost:4000/graphql"
+});
 
 render(
-    <Router><App /></Router>,
+    <ApolloProvider client={client}>
+        <Router>
+            <App />
+        </Router>
+    </ApolloProvider>,
     document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
