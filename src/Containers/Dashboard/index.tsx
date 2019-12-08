491ba5f2056e359a54f54035811f22ea4e5f336e
bboystatix <statix231@hotmail.com>
2019-12-08 08:05:27 +0800

    Set up basic subscription hook for clickBroadcast

--- src/Containers/Dashboard/index.tsx
+++ src/Containers/Dashboard/index.tsx
@@ -1,8 +1,21 @@
 import React, {FC} from "react";
 import Chart from "../../Components/Chart";
 import ClickCounter from "../../Components/ClickCounter";
+import {gql} from "apollo-boost"
+import {useSubscription} from '@apollo/react-hooks';
+
+const CLICK_SUBSCRIPTION = gql`
+    subscription {
+        clickBroadcast {
+            type
+            timestamp
+        }
+    }
+`
 
 const Dashboard: FC = () => {
+    const {loading, error, data} = useSubscription(CLICK_SUBSCRIPTION, {onSubscriptionData: ({subscriptionData}) => console.log(subscriptionData)})
+
     return (
         <div className='dashboard'>
             Dashboard
