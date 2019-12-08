ebdc03531b02a7b60f88e87d240e5de39427e9ed
bboystatix <statix231@hotmail.com>
2019-12-08 20:06:40 +0800

    Create file for exporting populating chart with initialChartData along with its interface

---
+++ src/Containers/Dashboard/initialChartData.ts
@@ -0,0 +1,27 @@
+export interface ChartDataItem {
+    name: string,
+    orange: number,
+    blue: number,
+    black: number
+}
+
+export const initialChartData:ChartDataItem[] = [
+    {
+        name: '0', orange: 0, blue: 0, black: 0,
+    },
+    {
+        name: '1', orange: 0, blue: 0, black: 0,
+    },
+    {
+        name: '2', orange: 0, blue: 0, black: 0,
+    },
+    {
+        name: '3', orange: 0, blue: 0, black: 0,
+    },
+    {
+        name: '4', orange: 0, blue: 0, black: 0,
+    },
+    {
+        name: '5', orange: 0, blue: 0, black: 0,
+    }
+]
